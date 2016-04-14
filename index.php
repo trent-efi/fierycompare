f58003700521891519b1f4a98c38932b5f900c02
trent-efi <trent.efi@gmail.com>
2016-04-14 10:58:23 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    echo "HELLO YALL";
+?>
+
+<!DOCTYPE html5>
+<html lang="en-US">
+    <head>
+	<link rel="stylesheet" href="style.css">        
+        <title>FieryPerfmon: Graphical Compare</title> 
+    </head>
+    <body>
+    
+    </body>
+</html>
